5f559628fb4c76f6a1788e759aa94f0a7eaedb44
lee kiri <mango.man.e.ya@gmail.com>
2025-02-20 22:24:54 +0900

    feat: 라우터에 인증 기반 접근 제어 추가 (PrivateRoute 및 PublicRoute 컴포넌트 구현)

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,20 +1,41 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Signup from "../pages/Signup";
 import Login from "../pages/Login";
 import MyPage from "../pages/MyPage";
 import Header from "../components/Header";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
+// PrivateRoute : 로그인이 필요한 페이지에 접근할 수 있도록 하는 컴포넌트
+// 로그인이 되어있지 않은 사용자는 login 페이지로 리다이렉트
+const PrivateRoute = ({ element: Element, ...rest }) => {
+  const { isAuthenticated } = useContext(AuthContext);
+  return isAuthenticated ? <Element {...rest} /> : <Navigate to="/login" />;
+};
+
+// PublicRoute : 로그인이 필요없는 페이지에 접근할 수 있도록 하는 컴포넌트
+// 로그인이 되어있는 사용자는 mypage로 리다이렉트
+const PublicRoute = ({ element: Element, ...rest }) => {
+  const { isAuthenticated } = useContext(AuthContext);
+  return !isAuthenticated ? <Element {...rest} /> : <Navigate to="/mypage" />;
+};
 const Router = () => {
   return (
     <BrowserRouter>
       <Header />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/mypage" element={<MyPage />}></Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route
+          path="/signup"
+          element={<PublicRoute element={Signup} />}
+        ></Route>
+        <Route path="/login" element={<PublicRoute element={Login} />}></Route>
+        <Route
+          path="/mypage"
+          element={<PrivateRoute element={MyPage} />}
+        ></Route>
+      </Routes>
     </BrowserRouter>
   );
 };
