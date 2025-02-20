338be4fa7ba92d341b2662f8e10a22d67f927de7
lee kiri <mango.man.e.ya@gmail.com>
2025-02-20 19:30:42 +0900

    feat: 인증 API 기능 추가 (회원가입, 로그인, 사용자 정보 확인, 프로필 변경)

---
+++ src/api/auth.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const API_URL = "https://www.nbcamp-react-auth.link";
+
+// 회원가입 - post /register userData : "id", "password", "nickname" / response : "message"("회원가입성공"), "success" : true
+export const register = async (userData) => {
+  const response = await axios.post(`${API_URL}/register`, userData);
+  return response.data;
+};
+
+//로그인 - post /login userData : "id", "password" / response : "accessToken","userId", "success", "avatar", "nickname"
+export const login = async (userData) => {
+  const response = await axios.post(`${API_URL}/login?expiresIn=10m`, userData);
+  return response.data;
+};
+
+//회원정보 확인 - get /user header: "Authorization": "bearer token" / response : id, nickname, avatar, success
+export const getUserProfile = async (token) => {
+  const response = await axios.get(`${API_URL}/user`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
+
+//프로필 변경 - patch /profile header : "Authorization": "bearer token" / formData : "nickname" / response : "avatar", "nickname", "message", "success"
+export const updateProfile = async (formData, token) => {
+  const response = await axios.patch(`${API_URL}/profile`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
