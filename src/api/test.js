190e28d53e7f34746c0e022a5fec8735255bc8b7
lee kiri <mango.man.e.ya@gmail.com>
2025-02-24 17:26:44 +0900

    feat: API 호출에 오류 처리 추가 및 사용자 알림 기능 구현

--- src/api/test.js
+++ src/api/test.js
@@ -3,30 +3,52 @@ import { jsonApi } from "./axios";
 
 // 데이터 읽어오기 (get) - 데이터베이스에 있는 모든 결과들 불러오기
 export const getTestResults = async () => {
-  const response = await jsonApi.get('');
-  return response.data;
+  try {
+    const response = await jsonApi.get("");
+    return response.data;
+  } catch (error) {
+    console.error(error.response.data);
+    alert(error.response.data.message);
+  }
 };
 
 // 데이터 추가하기 (post) - mbti결과 + 유저 정보
 export const createTestResult = async (resultData) => {
-  const response = await jsonApi.post('',{
-    date: Date.now(),
-    userId: resultData.userId, 
-    nickname: resultData.nickname,
-    mbtiResult: resultData.mbtiResult,
-    visibility: true,
-  });
-  return response.data;
+  try {
+    const response = await jsonApi.post("", {
+      date: Date.now(),
+      writerId: resultData.userId,
+      nickname: resultData.nickname,
+      mbtiResult: resultData.mbtiResult,
+      visibility: true,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error.response.data);
+    alert(error.response.data.message);
+  }
 };
 
 // 데이터 수정하기 (patch) / 결과 공개 여부 결정
-export const updateTestResultVisibility = async (id, visibility) => {
-  const response = await jsonApi.patch(`/${id}`, { visibility });
-  return response.data;
+export const updateTestResultVisibility = async (result) => {
+  try {
+    const response = await jsonApi.patch(`/${result.id}`, {
+      visibility: !result.visibility,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error.response.data);
+    alert(error.response.data.message);
+  }
 };
 
 // 데이터 삭제하기 (delete)
 export const deleteTestResult = async (id) => {
-  const response = await jsonApi.delete(`/${id}`);
-  return response.data;
+  try {
+    const response = await jsonApi.delete(`/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(error.response.data);
+    alert(error.response.data.message);
+  }
 };
